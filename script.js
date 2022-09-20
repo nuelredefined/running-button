149931a31ba88a377a2c8a60ad27ea9c9d343404
nuelredefined <mannynuelo@gmail.com>
2022-09-20 21:01:42 +0100

    init

---
+++ script.js
@@ -0,0 +1,14 @@
+const buttonHeight = 30;
+const buttonWidth = 150;
+const maxWidth = window.innerWidth - buttonWidth; 
+const maxHeight = window.innerHeight - buttonHeight; 
+window.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('button');
+
+    button.addEventListener("click", () => alert('click!'))
+    button.addEventListener("mouseover", () => {
+        button.style.left = Math.floor(Math.random() * (maxWidth + 1)) + 'px';
+        button.style.top = Math.floor(Math.random() * (maxHeight + 1)) + 'px';
+    })
+
+});
